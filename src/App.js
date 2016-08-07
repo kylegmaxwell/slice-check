2eb1eb4b266f75c3642863628efa33422c1cf695
Kyle <kyle.g.maxwell@gmail.com>
2016-08-06 20:40:12 -0700

    Mesh upload and physical material
    Added html and js to load stl files from user. Also changed material to
    use MeshPhysicalMaterial (subclass of MeshStandardMaterial).

--- src/App.js
+++ src/App.js
@@ -10,15 +10,16 @@ function App() {
 /**
  * loadStl - Function to load an stl file for rendering as a mesh
  */
-App.prototype.loadStl = function() {
+App.prototype.loadStl = function(stlUrl) {
     var _this = this;
-    this.loader.load( 'data/tissue.stl', function ( geometry ) {
-        // var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-		var material = new THREE.MeshPhongMaterial( { color: 0xff5533, specular: 0x111111, shininess: 200 } );
+    this.loader.load( stlUrl, function ( geometry ) {
+        geometry.computeBoundingSphere(); // needed for focus
+		var material = new THREE.MeshPhysicalMaterial( { color: 0xff5533, roughness: 0.7, metalness: 0.6 } );
 		var mesh = new THREE.Mesh( geometry, material );
 		// mesh.castShadow = true;
 		// mesh.receiveShadow = true;
 		_this.scene.add( mesh );
+        _this.controls.focus(mesh, true);
         _this.render();
 	} );
 };
@@ -33,17 +34,20 @@ App.prototype.initThree = function(container) {
     var width = Math.max(window.innerWidth/2,600);
     var height = Math.max(window.innerHeight/2,600);
 	this.camera = new THREE.PerspectiveCamera( 75, width/height, 0.1, 10000 );
-
-	this.renderer = new THREE.WebGLRenderer();
-    // this.renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
+	this.renderer = new THREE.WebGLRenderer({antialias:true});
 	this.renderer.setSize( width, height );
 	container.appendChild( this.renderer.domElement );
 	this.camera.position.z = -300;
     this.camera.lookAt(new THREE.Vector3(-35.031795501708984, 197.1667938232422, -204.77346801757812));
 
-    this.addLights();
+    this.controls = new THREE.EditorControls( this.camera, container );
+    var _this = this;
+    this.controls.addEventListener('change', function () {
+        _this.render();
+    });
 
-    this.loadStl();
+    this.addLights();
+    this.loadStl('data/tissue.stl');
 	this.render();
 };
 
@@ -52,10 +56,10 @@ App.prototype.initThree = function(container) {
  * addLights - Add some lights to the three.js scene
  */
 App.prototype.addLights = function() {
-    var directionalLight1 = new THREE.DirectionalLight( 0xffffff, 0.5 );
+    var directionalLight1 = new THREE.DirectionalLight( 0xffffff, 0.8 );
     directionalLight1.position.set( 1, -1, 0 );
     this.scene.add( directionalLight1 );
-    var directionalLight2 = new THREE.DirectionalLight( 0xffffff, 0.5 );
+    var directionalLight2 = new THREE.DirectionalLight( 0xffffff, 0.8 );
     directionalLight2.position.set( 0, 1, 0 );
     this.scene.add( directionalLight2 );
     var light = new THREE.AmbientLight( 0x404040 ); // soft white light
--- src/main.js
+++ src/main.js
@@ -7,3 +7,33 @@ function handleLoad() {
     app = new App();
     app.initThree(viewport);
 }
+
+/**
+ * loadMesh - Load a stl mesh when the file is selected
+ */
+function loadMesh() {
+    var selector = document.querySelector('#meshFile');
+    var files = selector.files;
+    for (var i=0;i<files.length;i++) {
+        if (files[i].indexOf('.stl')!==-1) {
+            loadFile(files[i]);
+        } else {
+            console.log('Unknown file',files[i]);
+        }
+    }
+}
+
+
+/**
+ * loadFile - Load an stl file to render
+ * @param {File} file Handle to a local file on the user's computer
+ */
+function loadFile(file) {
+    var reader  = new FileReader();
+    reader.addEventListener("load", function () {
+        app.loadStl(reader.result);
+    }, false);
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+}
