d8be6b46748753b898f2375d17c3e551cb8b9bc1
Kyle <kyle.g.maxwell@gmail.com>
2016-08-10 21:41:03 -0700

    Images loading and aligned
    Now loading DICOM images as 3D textures and positioning them using
    ImagePositionPatient

--- src/App.js
+++ src/App.js
@@ -7,20 +7,97 @@ function App() {
     this.loader = new THREE.STLLoader();
 }
 
+
+/**
+ * Add an object and prepare it for rendering
+ *
+ * @param {THREE.Object3D} obj    The new object
+ * @param {THREE.Object3D} parent The container
+ */
+App.prototype.addObj = function(obj, parent) {
+    if (obj.geometry) {
+        obj.geometry.computeBoundingSphere(); // needed for focus
+    }
+    parent.add( obj );
+    // THREE.EditorControls is buggy in determining the center point, so focus is disabled here
+    // this.controls.focus(parent, true);
+    this.render();
+};
+
+
+/**
+ * Load a dicom texture and create a quad to render it in 3D
+ *
+ * @param {String} imgUrl   Path to image data that can render on a canvas
+ * @param {Object} metadata DICOM header converted to human readable properties
+ *
+ * @return {type} Description
+ */
+App.prototype.loadTexture = function(imgUrl, metadata) {
+	var textureLoader = new THREE.TextureLoader();
+    var _this = this;
+	var texture1 = textureLoader.load( imgUrl, function () {
+        _this.render();
+    } );
+    var width = 512;
+    var height = 512;
+    var pixelWidth = 1.0;
+    var pixelHeight = 1.0;
+    console.log(metadata);
+    var offset = [0,0,0];
+    if (metadata) {
+        width = metadata.columns ? metadata.columns : width;
+        height = metadata.rows ? metadata.rows : height;
+        if (metadata.pixelSpacing) {
+            var dims = metadata.pixelSpacing.split('\\');
+            pixelWidth = parseFloat(dims[0]);
+            pixelHeight = parseFloat(dims[1]);
+            width = Math.floor(width*pixelWidth);
+            height = Math.floor(height*pixelHeight);
+        }
+        offset[0] += metadata.imagePositionPatient[0];
+        offset[1] += metadata.imagePositionPatient[1];
+        offset[2] += metadata.imagePositionPatient[2];
+
+    }
+    // Texture card geometry
+	var material = new THREE.MeshStandardMaterial( {
+        emissive: 0xffffff,
+        emissiveMap: texture1,
+        metalness: 0,
+        roughness: 0.6,
+        side: THREE.DoubleSide
+     } );
+    var geometry = new THREE.PlaneBufferGeometry( width, height, 1, 1 );
+    var mesh = new THREE.Mesh( geometry, material );
+    // Still figuring this out, perhaps the uv's should go 0 to 1-pixel?
+    var OFFSET_HACK = 2;
+    mesh.position.x += OFFSET_HACK + offset[0] * pixelWidth;
+    mesh.position.y -= offset[1] + height / 2;
+    mesh.position.z += offset[2];
+
+    mesh.rotation.y = Math.PI;
+
+    this.addObj(mesh, this.slicesObj);
+};
+
 /**
  * loadStl - Function to load an stl file for rendering as a mesh
  */
 App.prototype.loadStl = function(stlUrl) {
     var _this = this;
     this.loader.load( stlUrl, function ( geometry ) {
-        geometry.computeBoundingSphere(); // needed for focus
-		var material = new THREE.MeshPhysicalMaterial( { color: 0xff5533, roughness: 0.7, metalness: 0.6 } );
+		var material = new THREE.MeshPhysicalMaterial( {
+            color: 0xff5533,
+            transparent: true,
+            opacity: 0.9,
+            roughness: 0.7,
+            metalness: 0.6
+        } );
 		var mesh = new THREE.Mesh( geometry, material );
-		// mesh.castShadow = true;
-		// mesh.receiveShadow = true;
-		_this.scene.add( mesh );
+        _this.addObj(mesh, _this.meshesObj);
         _this.controls.focus(mesh, true);
-        _this.render();
+
 	} );
 };
 
@@ -31,20 +108,33 @@ App.prototype.loadStl = function(stlUrl) {
  */
 App.prototype.initThree = function(container) {
     this.scene = new THREE.Scene();
+    this.meshesObj = new THREE.Scene();
+    this.scene.add(this.meshesObj);
+    this.slicesObj = new THREE.Scene();
+    this.scene.add(this.slicesObj);
     var width = Math.max(window.innerWidth/2,600);
-    var height = Math.max(window.innerHeight/2,600);
+    var height = Math.max(window.innerHeight/2,500);
 	this.camera = new THREE.PerspectiveCamera( 75, width/height, 0.1, 10000 );
-	this.renderer = new THREE.WebGLRenderer({antialias:true});
+	this.renderer = new THREE.WebGLRenderer({
+        antialias:true
+    });
+    this.renderer.sortObjects = false;
+    this.renderer.setClearColor( 0x000000 );
 	this.renderer.setSize( width, height );
 	container.appendChild( this.renderer.domElement );
-	this.camera.position.z = -300;
-    this.camera.lookAt(new THREE.Vector3(-35.031795501708984, 197.1667938232422, -204.77346801757812));
-
+    this.camera.position.x = 300;
+    this.camera.position.y = 300;
+	this.camera.position.z = 300;
     this.controls = new THREE.EditorControls( this.camera, container );
+
     var _this = this;
     this.controls.addEventListener('change', function () {
         _this.render();
     });
+
+    // Keep a separate scene so that this doesnt affect focusing of controls
+    var axisHelper = new THREE.AxisHelper( 100 );
+    this.scene.add( axisHelper );
 
     this.addLights();
     this.loadStl('data/tissue.stl');
@@ -56,12 +146,22 @@ App.prototype.initThree = function(container) {
  * addLights - Add some lights to the three.js scene
  */
 App.prototype.addLights = function() {
-    var directionalLight1 = new THREE.DirectionalLight( 0xffffff, 0.8 );
+    var directionalLight1 = new THREE.DirectionalLight( 0xffffff, 0.5 );
     directionalLight1.position.set( 1, -1, 0 );
     this.scene.add( directionalLight1 );
+
     var directionalLight2 = new THREE.DirectionalLight( 0xffffff, 0.8 );
     directionalLight2.position.set( 0, 1, 0 );
     this.scene.add( directionalLight2 );
+
+    var directionalLight3 = new THREE.DirectionalLight( 0xffffff, 0.5 );
+    directionalLight3.position.set( -1, -1, 1 );
+    this.scene.add( directionalLight3 );
+
+    var directionalLight4 = new THREE.DirectionalLight( 0xffffff, 0.5 );
+    directionalLight4.position.set( 0, 0, -1 );
+    this.scene.add( directionalLight4 );
+
     var light = new THREE.AmbientLight( 0x404040 ); // soft white light
     this.scene.add( light );
 };
--- src/main.js
+++ src/main.js
@@ -1,11 +1,30 @@
 'use-strict';
-console.log("main");
 
+// Appliation entry point for object oriented model
 var app;
+
+/**
+ * Initialize the app when the page first loads
+ */
 function handleLoad() {
     var viewport = document.querySelector('#viewport');
     app = new App();
     app.initThree(viewport);
+    initHeaderMap();
+}
+// Data structge for DICOM header offset hash to description mapping
+var headerMap = {};
+/**
+ * Parse the header string webpage into a Javascript map
+ */
+function initHeaderMap() {
+    var keys = headerKeys.split('\n');
+    for (var i=0;i<keys.length;i++) {
+        var key = keys[i];
+        if (key[0]!=='(') continue;
+        var id = 'x'+key.substr(1,4)+key.substr(6,4);
+        headerMap[id] = key;
+    }
 }
 
 /**
@@ -54,14 +73,15 @@ function loadDcmFile(file) {
 var loaded = false;
 function loadAndViewImage(imageId) {
     var element = $('#dicomImage').get(0);
-    cornerstone.enable(element);
-    //try {
+    if (!loaded) {
+        cornerstone.enable(element);
+    }
     var start = new Date().getTime();
     cornerstone.loadImage(imageId).then(function(image) {
-        console.log(image);
         var viewport = cornerstone.getDefaultViewportForImage(element, image);
-        // $('#toggleModalityLUT').attr("checked",viewport.modalityLUT !== undefined);
-        // $('#toggleVOILUT').attr("checked",viewport.voiLUT !== undefined);
+        var metadata = {};
+        metadata.toggleModalityLUT = viewport.modalityLUT !== undefined;
+        metadata.toggleVOILUT = viewport.voiLUT !== undefined;
         cornerstone.displayImage(element, image, viewport);
         if(loaded === false) {
             cornerstoneTools.mouseInput.enable(element);
@@ -99,32 +119,72 @@ function loadAndViewImage(imageId) {
             return value + (value === 0 ? ' (pixel)' : ' (plane)');
         }
 
-        // $('#transferSyntax').text(getTransferSyntax());
-        // $('#sopClass').text(getSopClass());
-        // $('#samplesPerPixel').text(image.data.uint16('x00280002'));
-        // $('#photometricInterpretation').text(image.data.string('x00280004'));
-        // $('#numberOfFrames').text(image.data.string('x00280008'));
-        // $('#planarConfiguration').text(getPlanarConfiguration());
-        // $('#rows').text(image.data.uint16('x00280010'));
-        // $('#columns').text(image.data.uint16('x00280011'));
-        // $('#pixelSpacing').text(image.data.string('x00280030'));
-        // $('#bitsAllocated').text(image.data.uint16('x00280100'));
-        // $('#bitsStored').text(image.data.uint16('x00280101'));
-        // $('#highBit').text(image.data.uint16('x00280102'));
-        // $('#pixelRepresentation').text(getPixelRepresentation());
-        // $('#windowCenter').text(image.data.string('x00281050'));
-        // $('#windowWidth').text(image.data.string('x00281051'));
-        // $('#rescaleIntercept').text(image.data.string('x00281052'));
-        // $('#rescaleSlope').text(image.data.string('x00281053'));
-        // $('#basicOffsetTable').text(image.data.elements.x7fe00010.basicOffsetTable ? image.data.elements.x7fe00010.basicOffsetTable.length : '');
-        // $('#fragments').text(image.data.elements.x7fe00010.fragments ? image.data.elements.x7fe00010.fragments.length : '');
-        // $('#minStoredPixelValue').text(image.minPixelValue);
-        // $('#maxStoredPixelValue').text(image.maxPixelValue);
+        function getImagePositionPatient() {
+            var xyz = image.data.string('x00200032').split('\\');
+            var pos = [parseFloat(xyz[0]),parseFloat(xyz[1]),parseFloat(xyz[2])];
+            return pos;
+        }
+
+        function getImageOrientationPatient() {
+            var xyz = image.data.string('x00200037').split('\\');
+            return [
+                [parseFloat(xyz[0]),parseFloat(xyz[1]),parseFloat(xyz[2])],
+                [parseFloat(xyz[3]),parseFloat(xyz[4]),parseFloat(xyz[5])]
+            ];
+        }
+
+        metadata.transferSyntax = getTransferSyntax();
+        metadata.sopClass = getSopClass();
+        metadata.samplesPerPixel = image.data.uint16('x00280002');
+        metadata.photometricInterpretation = image.data.string('x00280004');
+        metadata.numberOfFrames = image.data.string('x00280008');
+        metadata.planarConfiguration = getPlanarConfiguration();
+        metadata.rows = image.data.uint16('x00280010');
+        metadata.columns = image.data.uint16('x00280011');
+        metadata.pixelSpacing = image.data.string('x00280030');
+        metadata.pixelSpacingSequence = image.data.string('x004008D8');
+        metadata.bitsAllocated = image.data.uint16('x00280100');
+        metadata.bitsStored = image.data.uint16('x00280101');
+        metadata.highBit = image.data.uint16('x00280102');
+        metadata.pixelRepresentation = getPixelRepresentation();
+        metadata.windowCenter = image.data.string('x00281050');
+        metadata.windowWidth = image.data.string('x00281051');
+        metadata.windowExplanation = image.data.string('x00281055');
+        metadata.rescaleIntercept = image.data.string('x00281052');
+        metadata.rescaleSlope = image.data.string('x00281053');
+        metadata.basicOffsetTable = image.data.elements.x7fe00010.basicOffsetTable ? image.data.elements.x7fe00010.basicOffsetTable.length : '';
+        metadata.fragments = image.data.elements.x7fe00010.fragments ? image.data.elements.x7fe00010.fragments.length : '';
+        metadata.minStoredPixelValue = image.minPixelValue;
+        metadata.maxStoredPixelValue = image.maxPixelValue;
+        metadata.physicalUnitsX = image.data.string('x00186024');
+        metadata.referencePixelX0 = image.data.string('x00186020');
+        metadata.imagePositionPatient = getImagePositionPatient();
+        metadata.imageOrientationPatient = getImageOrientationPatient();
+        console.log(metadata.imagePositionPatient);
+        console.log(JSON.stringify(metadata.imageOrientationPatient));
         var end = new Date().getTime();
         var time = end - start;
-        console.log('load time',time+'ms');
-        // $('#loadTime').text(time + "ms");
+        metadata.loadTime = time+'ms';
+        app.loadTexture(element.querySelector('canvas').toDataURL(), metadata);
+        // printData(image);
     }, function(err) {
         console.error(err);
     });
+}
+
+
+/**
+ * Print all the header data in various formats for debugging
+ *
+ * @param {cornerstone image} image Loaded from DICOM
+ */
+function printData(image) {
+    var keys = Object.keys(image.data.elements);
+    for (var i=0;i<keys.length;i++) {
+        var key = keys[i];
+        console.log(headerMap[key]+key);
+        console.log('string: '+image.data.string(key));
+        console.log('intString: '+image.data.intString(key));
+        console.log('float: '+image.data.float(key));
+    }
 }
